0436a2fa32c6f66f46d3563e41ebc4518cbb034e
Ksu <ksuha.golub1@gmail.com>
2023-06-08 02:16:21 +0300

    Add SelectionSort

---
+++ src/SelectionSort/SelectionSort.java
@@ -0,0 +1,26 @@
+package SelectionSort;
+
+import Helper.ArrayHelper;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        ArrayHelper h = new ArrayHelper();
+        h.ArrayRecipient();
+        int length = h.array.length;
+        int addressOfStart = 0;
+        int min, addressOfMin;
+        while (length-1 != addressOfStart) {
+            min = h.array[addressOfStart];
+            addressOfMin = addressOfStart;
+            for (int i = addressOfStart+1; i < length; i++)
+                if (h.array[i] < min) {
+                    min = h.array[i];
+                    addressOfMin = i;
+                }
+            if (addressOfMin != addressOfStart) h.ArrayShiftElements(addressOfMin, addressOfStart);
+            addressOfStart++;
+        }
+
+        h.ArraySender();
+    }
+}
